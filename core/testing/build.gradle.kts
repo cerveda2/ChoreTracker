a23541e99f465ddaac392ff73af2898606b52f84
David Cervenka <dadacervenka@gmail.com>
2026-03-29 20:47:57 +0200

    Remove testing module

--- core/testing/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("choretracker.kotlin.library")
-}
-
-dependencies {
-    implementation(project(":core:data-contract"))
-    implementation(project(":core:model"))
-    implementation(project(":core:common"))
-}
